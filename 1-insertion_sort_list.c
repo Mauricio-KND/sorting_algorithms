2e181406f7fc311d5b8f7e8f7006e46c46ab9e3a
Mauricio-KND <1299@holbertonschool.com>
2020-02-03 05:46:37 +0000

    Commit of task 1. Bety Fixed.

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts dbly linkd list of ints in ascending order.
+ * @list: Doubly linked list.
+ * Return: Nothing.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *New;
+
+	if ((*list)->next == NULL || *list == NULL || list == NULL)
+		return;
+	New = (*list)->next;
+	while (New != NULL)
+	{
+		while (New->prev != NULL && New->n < New->prev->n)
+		{
+			New->prev->next = New->next;
+			if (New->next != NULL)
+				New->next->prev = New->prev;
+			New->next = New->prev;
+			New->prev = New->prev->prev;
+			New->next->prev = New;
+			if (New->prev == NULL)
+				*list = New;
+			else
+				New->prev->next = New;
+			print_list(*list);
+		}
+		New = New->next;
+	}
+}
