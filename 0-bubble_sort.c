5ade2f4a492970bd3aa0b0d48362d313664c5c92
Mauricio-KND <1299@holbertonschool.com>
2020-02-03 04:55:19 +0000

    Fixing Betty Style.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,23 +1,23 @@
 #include "sort.h"
 /**
- *bubble_sort - Sorts an array of integers in ascending order. 
+ *bubble_sort - Sorts an array of integers in ascending order.
  * @array: Array of integers.
  * @size: Size of the array.
  * Return: Nothing.
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t a, b;
+	size_t a, b;
 
-    if (size < 2)
-            return;
-    for (a = 0; a < size - 1; a++)
-        for (b = 0; b < size - 1; b++)
-            if (array [b] > array [b + 1])
-            {
-                array[b] += array[b + 1];
-                array[b + 1] = array[b] - array[b + 1];
-                array[b] -= array[b + 1];
-                print_array(array, size);
-            } 
+	if (size < 2)
+		return;
+	for (a = 0; a < size - 1; a++)
+		for (b = 0; b < size - 1; b++)
+			if (array[b] > array[b + 1])
+			{
+				array[b] += array[b + 1];
+				array[b + 1] = array[b] - array[b + 1];
+				array[b] -= array[b + 1];
+				print_array(array, size);
+			}
 }
