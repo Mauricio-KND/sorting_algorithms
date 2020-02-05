27940bb8c9b5923c4a224e806046c4b2d66db311
Mauricio-KND <1299@holbertonschool.com>
2020-02-05 20:05:46 +0000

    Commit of task 3.

--- sort.h
+++ sort.h
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_array - Prints an array of integers
+ * struct listint_s - Doubly linked list node
  *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
